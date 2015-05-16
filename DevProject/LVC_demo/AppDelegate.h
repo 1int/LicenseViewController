d3fa623f0af4f0bfb46c48a68dce845bb725f868
Pavel Lint <1int@list.ru>
2015-05-16 15:12:55 +0300

    merge

---
+++ DevProject/LVC_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LVC_demo
+//
+//  Created by Pavel Lint on 5/15/15.
+//  Copyright (c) 2015 Pavel Lint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DevProject/LVC_demo/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  LVC_demo
+//
+//  Created by Pavel Lint on 5/15/15.
+//  Copyright (c) 2015 Pavel Lint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+-(IBAction) show: (id) sender;
+
+
+
+@end
+
